913f42bc4177234032c1ae49e2e2d40f18202213
Austin Steward & Eric Johnson <student@epicodus.com>
2014-07-22 15:11:27 -0700

    refactor :haircut:

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -12,72 +12,14 @@ def numerals_counter (input_num)
   input_array[1] = input_array[1] * 100
   input_array[2] = input_array[2] * 10
   res_array = []
-  num_hash = {
-    "I" => 1,
-    "V" => 5,
-    "X" => 10,
-    "L" => 50,
-    "C" => 100,
-    "D" => 500,
-    "M" => 1000
-  }
-
-  while input_array[0] >= 1000
-    input_array[0] -= 1000
-    res_array.push("M")
-  end
-  while input_array[1] >= 500
-    if input_array[1] == 900
-      input_array[1] -= 900
-      res_array.push("CM")
-    else
-    input_array[1] -= 500
-    res_array.push("D")
-    end
-  end
-  while input_array[1] >= 100
-    if input_array[1] == 400
-      input_array[1] -= 400
-      res_array.push("CD")
-    else
-    input_array[1] -= 100
-    res_array.push("C")
-    end
-  end
-  while input_array[2] >= 50
-    if input_array[2] == 90
-      input_array[2] -= 90
-      res_array.push("XC")
-    else
-    input_array[2] -= 50
-    res_array.push("L")
-    end
-  end
-  while input_array[2] >= 10
-    if input_array[2] == 40
-      input_array[2] -= 40
-      res_array.push("XL")
-    else
-    input_array[2] -= 10
-    res_array.push("X")
-    end
-  end
-  while input_array[3] >= 5
-    if input_array[3] == 9
-      input_array[3] -= 9
-      res_array.push("IX")
-    else
-    input_array[3] -= 5
-    res_array.push("V")
-    end
-  end
-  while input_array[3] >= 1
-    if input_array[3] == 4
-      input_array[3] -= 4
-      res_array.push("IV")
-    else
-    input_array[3] -= 1
-    res_array.push("I")
+  numerals_array = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+  numbers_array = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  input_array.each do |i|
+    0.upto(12) do |z|
+      while i >= numbers_array[z]
+        res_array.push(numerals_array[z])
+        i -= numbers_array[z]
+      end
     end
   end
   res_array.join("")
