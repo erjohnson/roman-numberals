c30ccf31b86899b8641d0171f772213f3fbe40b4
Austin Steward & Eric Johnson <student@epicodus.com>
2014-07-22 14:40:27 -0700

    :trophy: add support for input up to 3999

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,13 +1,17 @@
 def numerals_counter (input_num)
-
-  num_array = [1, 5, 10, 50, 100, 500, 1000].reverse
-
-  char_array = ["I","V","X","L","C","D","M"].reverse
-
+  str_length = 0
+  if input_num.length < 4
+    str_length = 4 - input_num.length
+    1.upto(str_length) { input_num = "0" + input_num }
+  end
+  input_array = input_num.split("")
+  0.upto(3) do |i|
+    input_array[i] = input_array[i].to_i
+  end
+  input_array[0] = input_array[0] * 1000
+  input_array[1] = input_array[1] * 100
+  input_array[2] = input_array[2] * 10
   res_array = []
-
-  i = 0
-
   num_hash = {
     "I" => 1,
     "V" => 5,
@@ -18,12 +22,63 @@ def numerals_counter (input_num)
     "M" => 1000
   }
 
-  char_array.each do
-    while input_num >= num_array[i]
-      input_num -= num_array[i]
-      res_array.push(char_array[i])
+  while input_array[0] >= 1000
+    input_array[0] -= 1000
+    res_array.push("M")
+  end
+  while input_array[1] >= 500
+    if input_array[1] == 900
+      input_array[1] -= 900
+      res_array.push("CM")
+    else
+    input_array[1] -= 500
+    res_array.push("D")
     end
-    i+=1
+  end
+  while input_array[1] >= 100
+    if input_array[1] == 400
+      input_array[1] -= 400
+      res_array.push("CD")
+    else
+    input_array[1] -= 100
+    res_array.push("C")
+    end
+  end
+  while input_array[2] >= 50
+    if input_array[2] == 90
+      input_array[2] -= 90
+      res_array.push("XC")
+    else
+    input_array[2] -= 50
+    res_array.push("L")
+    end
+  end
+  while input_array[2] >= 10
+    if input_array[2] == 40
+      input_array[2] -= 40
+      res_array.push("XL")
+    else
+    input_array[2] -= 10
+    res_array.push("X")
+    end
+  end
+  while input_array[3] >= 5
+    if input_array[3] == 9
+      input_array[3] -= 9
+      res_array.push("IX")
+    else
+    input_array[3] -= 5
+    res_array.push("V")
+    end
+  end
+  while input_array[3] >= 1
+    if input_array[3] == 4
+      input_array[3] -= 4
+      res_array.push("IV")
+    else
+    input_array[3] -= 1
+    res_array.push("I")
+    end
   end
   res_array.join("")
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,10 +3,14 @@ require('roman_numerals')
 
 describe('numerals_counter') do
   it('Returns the Roman numeral version of a single digit') do
-    numerals_counter(1).should(eq("I"))
+    numerals_counter("1").should(eq("I"))
   end
 
   it('Returns the 2-character Roman numeral version of a single digit') do
-    numerals_counter(2).should(eq("II"))
+    numerals_counter("2").should(eq("II"))
+  end
+
+  it('Returns a multicharacter Roman numeral version of a number') do
+    numerals_counter("3999").should(eq("MMMCMXCIX"))
   end
 end
