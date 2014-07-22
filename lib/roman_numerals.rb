c53589e6f69a74eac653e067094110446db6e787
Austin Steward & Eric Johnson <student@epicodus.com>
2014-07-22 16:18:16 -0700

    :grimacing: add roman numeral to number functionality

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -24,3 +24,29 @@ def numerals_counter (input_num)
   end
   res_array.join("")
 end
+
+def reverse_counter (input_num)
+  numerals_array = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+  numbers_array = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  numerals_hash = {"M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1}
+  res_array = []
+  total = 0
+  input_array = input_num.split("")
+
+  input_array.each do |i|
+    res_array.push(numerals_hash.values_at(i).join().to_i)
+  end
+  0.upto((res_array.length - 1)) do |i|
+    if (res_array[i+1] != nil) && (res_array[i] != "Q")
+      if (res_array[i] < res_array[i+1])
+        total += (res_array[i+1] - res_array[i])
+        res_array[i+1] = "Q"
+      else
+        total += res_array[i]
+      end
+    elsif (res_array[i] != "Q")
+      total+= res_array[i]
+    end
+  end
+  total
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -14,3 +14,13 @@ describe('numerals_counter') do
     numerals_counter("3999").should(eq("MMMCMXCIX"))
   end
 end
+
+describe('reverse_counter') do
+  it('Returns the digit version of a Roman numeral') do
+    reverse_counter("I").should(eq(1))
+  end
+
+  it('Returns the digit version of a large Roman numeral') do
+    reverse_counter("MMMCMXCIX").should(eq(3999))
+  end
+end
